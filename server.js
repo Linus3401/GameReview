9e5f07e4598b26bf650c32c0af43f0e17c3e6eda
Linus <linus.m.72@gmail.com>
2025-12-30 21:19:59 +0100

    Initial commit - Express server and HomePage

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 3000;
+
+// Serve statiska filer från 'public'
+app.use(express.static('public'));
+
+// Route för startsidan
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('public/HomePage.html'));
+});
+
+// Starta servern
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
